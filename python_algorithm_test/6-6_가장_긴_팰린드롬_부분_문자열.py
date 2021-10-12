4b8ea5c9271ab168e33a2137e650cb723437582f
Youngwon Choi <one01011@gm.gist.ac.kr>
2021-10-12 15:02:00 +0900

    Create 6-6_가장_긴_팰린드롬_부분_문자열.py
    r

---
+++ python_algorithm_test/6-6_가장_긴_팰린드롬_부분_문자열.py
@@ -0,0 +1,8 @@
+def longest_palandrome(s:str):
+    
+    pass
+
+input1='babad'
+print("output1 : {}".format(longest_palandrome(input1)))
+input2='cbbd'
+print("output2 : {}".format(longest_palandrome(input2)))
