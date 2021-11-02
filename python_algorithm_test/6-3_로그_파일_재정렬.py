8c5c5a2a8639cf5ba1d3b716ae437c9e37a69816
Youngwon Choi <one01011@gm.gist.ac.kr>
2021-11-02 22:55:08 +0900

    fdf

--- python_algorithm_test/6-3_로그_파일_재정렬.py
+++ python_algorithm_test/6-3_로그_파일_재정렬.py
@@ -1,19 +1,19 @@
-def reorder_logfile(l:list):
-    letters, digits=[],[]
+
+def reorder_log(logs:list):
+    letters=[]
+    digits=[]
     for log in logs:
         if log.split()[1].isdigit():
             digits.append(log)
         else:
             letters.append(log)
-
-    letters.sort(key=lambda x:(x.split()[1:], x.split()[0]))
+    letters.sort(key=lambda x: (x.split()[1:], x.split()[0]))
     return letters + digits
 
 
 
 
-
-
 logs=['dig1 8 1 5 1 ','let1 art can', 'dig2 3 6', 'let own kilt dig', 'let3 art zero']
-result=reorder_logfile(logs)
+result=reorder_log(logs)
 print(result)
+
