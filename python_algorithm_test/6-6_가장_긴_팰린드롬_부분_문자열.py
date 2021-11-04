4820e09bdb3a9c2d58126a1abc7251c2d0d93fac
Youngwon Choi <one01011@gm.gist.ac.kr>
2021-11-04 17:01:54 +0900

    대충 커밋

--- python_algorithm_test/6-6_가장_긴_팰린드롬_부분_문자열.py
+++ python_algorithm_test/6-6_가장_긴_팰린드롬_부분_문자열.py
@@ -1,8 +1,27 @@
 def longest_palandrome(s:str):
-    
-    pass
 
-input1='babad'
+    if len(s)<2 or s==s[::-1]:
+        return s
+    
+    def expand(left:int, right:int)-> str:
+        while left>=0 and right <len(s) and s[left]==s[right]:
+            left-=1
+            right+=1
+        return s[left+1:right]
+    
+    
+
+    result=''
+    for i in range(len(s)-1):
+        result=max(result,
+        expand(i,i+1),
+        expand(i,i+2),
+        key=len)
+    
+
+    return result
+
+input1='babadejfjefjfedksl'
 print("output1 : {}".format(longest_palandrome(input1)))
-input2='cbbd'
+input2='12345432'
 print("output2 : {}".format(longest_palandrome(input2)))
