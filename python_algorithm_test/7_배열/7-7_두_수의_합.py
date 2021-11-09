dcc684884f41fcf42f0fef5f86ab57255164f246
Youngwon Choi <one01011@gm.gist.ac.kr>
2021-11-09 14:00:44 +0900

    커밋 라스고

--- python_algorithm_test/7_배열/7-7_두_수의_합.py
+++ python_algorithm_test/7_배열/7-7_두_수의_합.py
@@ -1,11 +1,33 @@
-def find_sum_bf(nums:list, target:int)->list:
+def two_sum_bf(nums:list, target:int)->list:
     for i in range(len(nums)):
         for j in range(i+1,len(nums)):
             if nums[i]+nums[j]==target:
                 return [i,j]
-    return None
+    
+def two_sum_in(nums:list, target:int)->list:
+    for i, n in enumerate(nums):
+        complement= target-n
 
+        if complement in nums[i+1:]:
+            return [nums.index(n), nums[i+1:].index(complement)+(i+1)]
+
+def two_sum_dic(nums:list, target:int)->list:
+    nums_dic={}
+
+    for i, num in enumerate(nums):
+        nums_dic[num]=i
+
+    for i, num in enumerate(nums):
+        if target-num in nums_dic and i!=nums_dic[target-num]:
+            return(i, nums_dic[target-num])
+
+def two_sum_dic2(nums:list, target:int)->list:
+    nums_dic={}
+    for i, num in enumerate(nums):
+        if target-num in nums_dic and i!=nums_dic[target-num]:
+            return(i, nums_dic[target-num])
+        nums_dic[num]=i   
 
 nums=[2,2,7,15]
 target=9
-print(find_sum(nums,target))
+print(two_sum_dic2(nums,target))
