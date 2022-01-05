54d5743a162d77d2f732177cffee727b9e1d4747
Youngwon Choi <one01011@gm.gist.ac.kr>
2022-01-05 16:44:41 +0900

    수정커밋

--- python_algorithm_test/6_문자열_조작/6-5_그룹_애너그램.py
+++ python_algorithm_test/6_문자열_조작/6-5_그룹_애너그램.py
@@ -2,11 +2,12 @@ import collections
 
 def group_anagrams(words: list):
     anagrams=collections.defaultdict(list)
+
     for word in words:
         anagrams[''.join(sorted(word))].append(word)
     
 
-    return anagrams.values()
+    return list(anagrams.values())
 
 if __name__=='__main__':
     inp=['eat','tea','tan','ate','nat','bat']
