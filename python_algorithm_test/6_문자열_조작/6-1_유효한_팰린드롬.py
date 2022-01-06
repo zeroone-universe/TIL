6051c282d6b15368a3d0b674a628d42f3357b59f
Youngwon Choi <one01011@gm.gist.ac.kr>
2022-01-06 11:48:02 +0900

    6-1

--- python_algorithm_test/6_문자열_조작/6-1_유효한_팰린드롬.py
+++ python_algorithm_test/6_문자열_조작/6-1_유효한_팰린드롬.py
@@ -2,39 +2,19 @@ import time
 import collections
 import re
 
-def palindrome1(s):
-    strs=[]
+def isPalindrome(s):
+    s_list=collections.deque()
+        
     for char in s:
-        if char.isalnum():
-            strs.append(char.lower())
-
-
-    while len(strs)>1:
-        if strs.pop(0)!=strs.pop():
+        if char.isalnum() is True:
+            s_list.append(char.lower())
+            
+    while len(s_list)>1:
+        if s_list.popleft() !=s_list.pop():
             return False
-
     return True
-    
-def palindrome2(s: str) -> bool:
-    strs=collections.deque()
-    for char in s:
-        if char.isalnum():
-            strs.append(char.lower())
 
-
-    while len(strs)>1:
-        if strs.popleft()!=strs.pop():
-            return False
-
-    return True
-    
-def palindrome3(s):
-    s=s.lower()
-    s=re.sub('[^a-z0-9]','',s)
-
-    return s==s[::-1]
 if __name__=='__main__':
-    start = time.time()
-    a=palindrome2('A man, a plan, a canal: panama')
+    a=isPalindrome('A man, a plan, a canal: panama')
     print(a)
-    print("time :", time.time() - start)
+    
