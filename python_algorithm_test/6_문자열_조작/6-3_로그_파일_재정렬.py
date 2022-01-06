ba842e63ac72944d0bf58f985c1b87efe1c3744a
Youngwon Choi <one01011@gm.gist.ac.kr>
2022-01-06 14:32:28 +0900

    커밋

--- python_algorithm_test/6_문자열_조작/6-3_로그_파일_재정렬.py
+++ python_algorithm_test/6_문자열_조작/6-3_로그_파일_재정렬.py
@@ -1,19 +1,21 @@
-
-def reorder_log(logs:list):
-    letters=[]
-    digits=[]
+def reorderLogFiles(logs: list) -> list:
+    digit_log_list=[]
+    letter_log_list=[]
     for log in logs:
         if log.split()[1].isdigit():
-            digits.append(log)
-        else:
-            letters.append(log)
-    letters.sort(key=lambda x: (x.split()[1:], x.split()[0]))
-    return letters + digits
+            digit_log_list.append(log)
+        else :
+            letter_log_list.append(log)
+                
+    letter_log_list.sort(key=lambda x: (x.split()[1:], x.split()[0]))
+    ans=letter_log_list+digit_log_list
+    return ans
+        
 
 
 
 if __name__=='__main__':
     logs=['dig1 8 1 5 1 ','let1 art can', 'dig2 3 6', 'let own kilt dig', 'let3 art zero']
-    result=reorder_log(logs)
+    result=reorderLogFiles(logs)
     print(result)
 
--- python_algorithm_test/6_문자열_조작/6-4_가장_흔한_단어.py
+++ python_algorithm_test/6_문자열_조작/6-4_가장_흔한_단어.py
@@ -1,17 +1,18 @@
 import collections
 import re
 
-def mostcommon(paragraph, banned):
-    a=[]
-    for word in re.sub(r'[^\w]',' ',paragraph).lower().split():
+
+def mostCommonWord(paragraph: str, banned: list) -> str:
+    paragraph_list=[]
+    for word in re.sub(r'[^\w]', ' ', paragraph).lower().split():
         if word not in banned:
-            a.append(word)
+            paragraph_list.append(word)
+    counts=collections.Counter(paragraph_list)
     
-    
-    b=collections.Counter(a)
-    return b.most_common(1)[0][0]
+    return counts.most_common(1)[0][0]
+
 
 if __name__=='__main__':
     paragraph='Bob hit a ball, the hit BALL flew far after it was hit'
     banned= ['hit']
-    print(mostcommon(paragraph, banned))
+    print(mostCommonWord(paragraph, banned))
