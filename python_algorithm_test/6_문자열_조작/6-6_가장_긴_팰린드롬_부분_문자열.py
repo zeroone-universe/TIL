02ce61aa8e86041eccccfba7fc11b082ab9eb341
Youngwon Choi <one01011@gm.gist.ac.kr>
2022-01-09 13:50:38 +0900

    커밋

--- python_algorithm_test/6_문자열_조작/6-6_가장_긴_팰린드롬_부분_문자열.py
+++
@@ -1,9 +0,0 @@
-def longest_palandrome(s:str):
-
-    pass
-if __name__ == '__main__':
-
-    input1='babadejfjefjfedksl'
-    print("output1 : {}".format(longest_palandrome(input1)))
-    input2='12345432'
-    print("output2 : {}".format(longest_palandrome(input2)))
